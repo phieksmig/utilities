26f7c79fd40c408707389ccd28ef9765746db6ba
Philip Hjalmrud Ekströmer <philip.hjalmrud.ekstromer@migrationsverket.se>
2026-05-28 14:02:33 +0200

    feat: add links to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,37 @@
-import { Heading } from "@midas-ds/components";
-
+import { Heading, Text, LinkButton, ButtonGroup } from "@midas-ds/components";
+import styles from "./Home.module.css";
 export default function Home() {
   return (
-    <div>
-      <Heading isExpressive level={1}>
-        Midas utility app
-      </Heading>
-      <p>
-        En sida med bra-att-ha-saker för Designsystem. Byggd med Midas
-        komponenter för Designsystemteamet.
-      </p>
+    <div className={styles.mainContainer}>
+      <div>
+        <Heading enableMargins isExpressive level={1}>
+          Midas utility app
+        </Heading>
+        <Text>
+          En sida med bra-att-ha-saker för Designsystem. <br />
+          Byggd med Midas komponenter för Designsystemteamet.
+        </Text>
+      </div>
+      <ButtonGroup>
+        <LinkButton
+          href="https://designsystem.migrationsverket.se"
+          variant="primary"
+        >
+          Docwebben
+        </LinkButton>
+        <LinkButton
+          href="https://designsystem.migrationsverket.se/storybook/?path=/docs/components-intro--docs"
+          variant="secondary"
+        >
+          Storybook
+        </LinkButton>
+        <LinkButton
+          href="https://github.com/migrationsverket/midas"
+          variant="tertiary"
+        >
+          Github
+        </LinkButton>
+      </ButtonGroup>
     </div>
   );
 }
