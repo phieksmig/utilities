c62466a4019d2c55e30bdf945330a1e06815d75f
Philip Hjalmrud Ekströmer <philip.hjalmrud.ekstromer@migrationsverket.se>
2026-06-02 13:57:43 +0200

    feat: add hook for todo list

---
+++ src/hooks/useTodos.tsx
@@ -0,0 +1,33 @@
+// useTodos.ts
+import { useState } from "react";
+import { type Todo, type Priority } from "../types/types";
+
+export function useTodos() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodo = (title: string, description: string, priority: Priority) => {
+    const newTodo: Todo = {
+      id: crypto.randomUUID(),
+      title,
+      description: description || undefined,
+      isCompleted: false,
+      priority,
+      createdAt: new Date().toLocaleDateString(),
+    };
+    setTodos((prev) => [newTodo, ...prev]);
+  };
+
+  const toggleTodo = (id: string) => {
+    setTodos((prev) =>
+      prev.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo,
+      ),
+    );
+  };
+
+  const deleteTodo = (id: string) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  return { todos, addTodo, toggleTodo, deleteTodo };
+}
