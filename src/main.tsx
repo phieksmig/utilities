a78380631aac7428b7fd2e22062eae6d3a0ed5bd
Philip Hjalmrud Ekströmer <philip.hjalmrud.ekstromer@migrationsverket.se>
2026-06-02 11:22:45 +0200

    fix: fix import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router";
 import "./index.css";
 import "@midas-ds/components/default.css";
 import "@midas-ds/layout/default.css";
