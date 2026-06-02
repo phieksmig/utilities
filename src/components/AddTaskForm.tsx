9a276666b05246f716adcac70a9575c18fa08cd7
Philip Hjalmrud Ekströmer <philip.hjalmrud.ekstromer@migrationsverket.se>
2026-06-02 13:58:10 +0200

    feat: add component that lets the user add a todo item

---
+++ src/components/AddTaskForm.tsx
@@ -0,0 +1,46 @@
+import { Button, ButtonGroup, TextField } from "@midas-ds/components";
+import { useState } from "react";
+import type { Priority } from "../types/types";
+
+type AddTaskFormProps = {
+  addTodo: (title: string, description: string, priority: Priority) => void;
+};
+
+export const AddTaskForm = ({ addTodo }: AddTaskFormProps) => {
+  // Hooks
+
+  //States
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [priority, setPriority] = useState<Priority>("medium");
+
+  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!title.trim()) return;
+
+    addTodo(title, description, priority);
+
+    // Nollställ formulär
+    setTitle("");
+    setDescription("");
+    setPriority("medium");
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label="Lägg till uppgift"
+          value={title}
+          onChange={(value) => setTitle(value)}
+          required
+        />
+        <ButtonGroup>
+          <Button variant="primary" type="submit">
+            Lägg till
+          </Button>
+        </ButtonGroup>
+      </form>
+    </>
+  );
+};
