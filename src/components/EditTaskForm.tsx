7273d0a7a4d2aa189168d755908bfb555ae32f5d
Philip Hjalmrud Ekströmer <philip.hjalmrud.ekstromer@migrationsverket.se>
2026-06-11 22:49:15 +0200

    feat: make the tasks editable in the panel

--- src/components/EditTaskForm.tsx
+++ src/components/EditTaskForm.tsx
@@ -1,31 +1,70 @@
-import { TextArea, TextField } from "@midas-ds/components";
-import { useTodos } from "../hooks/useTodos";
+import { TextArea, TextField, Button } from "@midas-ds/components";
 import styles from "./EditTaskForm.module.css";
+import type { Todo } from "../types/types";
+import { Pen } from "lucide-react";
+import { useState, useEffect } from "react";
 
 interface EditTaskFormProps {
   todoId: string;
+  todos: Todo[];
+  editTitle: (id: string, title: string) => void;
+  editDescription: (id: string, description: string) => void;
 }
 
-export const EditTaskForm = ({ todoId }: EditTaskFormProps) => {
-  const { todos, editTitle, editDescription } = useTodos();
+export const EditTaskForm = ({
+  todoId,
+  todos,
+  editTitle,
+  editDescription,
+}: EditTaskFormProps) => {
   const todo = todos.find((item) => item.id === todoId);
+  const [editMode, setEditMode] = useState(false);
+  const [formTitle, setFormTitle] = useState(todo?.title || "");
+  const [formDescription, setFormDescription] = useState(
+    todo?.description || "",
+  );
 
   if (!todo) {
     return null;
   }
 
+  const handleEdit = () => {
+    setEditMode(true);
+  };
+
+  const handleSave = () => {
+    editTitle(todo.id, formTitle);
+    editDescription(todo.id, formDescription);
+    setEditMode(false);
+  };
+
+  if (editMode)
+    return (
+      <div className={styles.panelContainer}>
+        <TextField
+          label="Titel"
+          value={formTitle}
+          onChange={(value) => setFormTitle(value)}
+        />
+
+        <TextArea
+          label="Beskrivning"
+          value={formDescription}
+          onChange={(value) => setFormDescription(value)}
+        />
+
+        <Button variant="primary" onPress={handleSave}>
+          Spara
+        </Button>
+      </div>
+    );
+
   return (
     <div className={styles.panelContainer}>
-      <TextField
-        label="Titel"
-        value={todo.title}
-        onChange={(value) => editTitle(todo.id, value)}
-      />
-      <TextArea
-        label="Beskrivning"
-        value={todo.description}
-        onChange={(value) => editDescription(todo.id, value)}
-      />
+      <TextField isReadOnly label="Titel" value={formTitle} />
+
+      <TextArea isReadOnly label="Beskrivning" value={formDescription} />
+      <Button variant="icon" icon={Pen} onPress={handleEdit}></Button>
     </div>
   );
 };
